086a5f05f9e4c9c4ebee4793e0c089dd6fdb79cc
renderman21 <renderman21@gmail.com>
2022-12-27 23:29:34 +0800

    Shorten Words, added sounds, added main feature

--- main/ball.lua
+++ main/ball.lua
@@ -4,13 +4,15 @@ Ball = {}
 
 
 function Ball:load()
-   self.x = love.graphics.getWidth() / 2
-   self.y = love.graphics.getHeight() / 2
+   self.x = FULL_WINDOW_WIDTH/ 2
+   self.y = FULL_WINDOW_HEIGHT / 2
    self.width = 20
    self.height = 20
    self.speed = 500
    self.xVel = -self.speed
    self.yVel = 0
+   
+   self.sound = love.audio.newSource("assets/sfx/hit.ogg","stream")
 end
 
 
@@ -42,11 +44,27 @@ end
 ---@param entity any
 ---@param mod any
 function Ball:ballEntityHit(entity, mod)
+
+   Player.speed = Player.speed + 50
+   AI.speed = AI.speed + 50
+
+
+
+   self.speed = self.speed + 100
    self.xVel = self.speed * mod
    local middleBall = self.y + self.height / 2
    local middleEntity = entity.y + entity.height / 2
    local collisionPosition = middleBall - middleEntity
    self.yVel = collisionPosition * 5
+
+   self:playSound()
+
+end
+
+function Ball:playSound()
+   if not self.sound:isPlaying() then
+      love.audio.play(self.sound)
+   end 
 end
 
 ---Manage the score
@@ -57,7 +75,7 @@ function Ball:score()
       Score.ai = Score.ai + 1
    end
    --If passed on enemy
-   if self.x + self.width > love.graphics.getWidth() then
+   if self.x + self.width > FULL_WINDOW_WIDTH then
       self:ballReset(-1)
       Score.player = Score.player + 1
    end
@@ -66,8 +84,14 @@ end
 ---Resets according to which side had pass
 ---@param mod integer changes the direction of the velocity
 function Ball:ballReset(mod)
-   self.x = love.graphics.getWidth() / 2 - self.width / 2
-   self.y = love.graphics.getHeight() / 2 - self.height / 2
+   --Reset speed
+   self.speed = 500 
+   Player.speed = 500
+   AI.speed = 500
+
+
+   self.x = FULL_WINDOW_WIDTH/ 2 - self.width / 2
+   self.y = FULL_WINDOW_HEIGHT / 2 - self.height / 2
    self.yVel = 0
    self.xVel = mod * self.speed
 end
@@ -77,8 +101,8 @@ function Ball:wallBounce()
    if self.y < 0 then
       self.y = 0
       self.yVel = -self.yVel
-   elseif self.y + self.height > love.graphics.getHeight() then
-      self.y = love.graphics.getHeight() - self.height
+   elseif self.y + self.height > FULL_WINDOW_HEIGHT then
+      self.y = FULL_WINDOW_HEIGHT - self.height
       self.yVel = -self.yVel
    end
 end
@@ -92,5 +116,6 @@ end
 
 
 function Ball:draw()
+   love.graphics.setColor(255,255,255)
    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
 end
