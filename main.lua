cfd09c71e7bde5d2ea990c716923e10e3cb566c7
renderman21 <renderman21@gmail.com>
2022-12-27 20:48:25 +0800

    Added comment

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ require("player")
 require("ball")
 require("ai")
 
-
+---Load the entities 
 function love.load()
    Player:load()
    Ball:load()
@@ -11,14 +11,15 @@ function love.load()
    Score = {player = 0, ai = 0}
 end
 
-
+---Update the game by each frames
+---@param dt integer to adopt to the computer spec
 function love.update(dt)
    Player:update(dt)
    Ball:update(dt)
    AI:update(dt)
 end
 
-
+---Draw stuff on the screen
 function love.draw()
    Player:draw()
    Ball:draw()
@@ -28,7 +29,10 @@ function love.draw()
    love.graphics.print("Baddie: "..Score.ai, 1000, 50)
 end
 
-
+---Checks if there's collision between two entities
+---@param a table the first entity
+---@param b table the second entity
+---@return boolean
 function checkCollision(a, b)
    if a.x + a.width > b.x and a.x < b.x + b.width and a.y + a.height > b.y and a.y < b.y + b.height then
       return true
@@ -37,6 +41,8 @@ function checkCollision(a, b)
    end
 end
 
+---Checks if within the boundary of the screen
+---@param entity table the entity
 function CheckBoundaries(entity)
    if entity.y < 0 then
       entity.y = 0
